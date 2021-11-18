a31761d98143a15f18b9a3c8d742df77d7af7384
Franco Velasco <francovelasco10@hotmail.com>
2021-11-18 13:46:58 -0500

    ejercicio terminado sobre apis

--- script/script.js
+++ script/script.js
@@ -1,19 +1,46 @@
-const api_url = 'https://pokeapi.co/api/v2/pokemon/'
+const api_url = "https://pokeapi.co/api/v2/pokemon/";
 
-const form = document.getElementById('form')
-const search = document.getElementById('search')
-const main = document.getElementById('main')
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
 
 const getPokemon = async (url) => {
-    const res = await fetch(url)
-    const data = await res.json()
-    showPokemon(data);
+
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        showPokemon(data);
+    } catch {
+        swal.fire({
+            title: 'Algo falló',
+            text: 'Hubo un error en el servidor',
+            icon: 'error'
+        }) 
+    }
+
+
+};
+
+for (let i = 1; i <= 50; i++) {
+  getPokemon(api_url + i);
 }
-
-getPokemon(api_url + "1")
-
 
 const showPokemon = (data) => {
-    const {id, name, sprites, base_experience, types } = data
-    console.log(id, name, sprites, base_experience, types);
-}
+  const { id, name, sprites, base_experience, types } = data;
+
+  const pokemonCard = document.createElement("div");
+
+  pokemonCard.classList.add("movie");
+
+  pokemonCard.innerHTML = `
+      <img src="${sprites.front_default}" alt="imagenn_pokemon">
+      <div class="movie-info">
+          <h3>${name}</h3>
+          <span class="green">${base_experience}</span>
+      </div>
+      <div class="overview">
+          <h3>${types[0].type.name}</h3>
+      </div>
+  `;
+  main.appendChild(pokemonCard);
+};
